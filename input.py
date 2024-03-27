5b8ba60937733f48311a0519cb040d10258f6822
Gabriel Souza <95205382+GSnery@users.noreply.github.com>
2024-03-27 16:01:42 -0300

    Add files via upload

---
+++ input.py
@@ -0,0 +1,9 @@
+codigo = int(input(" digite o código do funcionario "))
+nome = input(" Digite o nome ")
+salario = float(input(" digite o salario do funcionario "))
+ativo = input("Funcionário ativo? ")
+
+print("codigo: %d"% codigo)
+print("nome: %s"% nome)
+print("salario: %.2f"% salario)
+print("Ativo: %r"% ativo)
